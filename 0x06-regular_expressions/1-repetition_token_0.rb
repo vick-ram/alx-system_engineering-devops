07b339ab427a3e98d69ebd709e0f109d5a6466a5
vick-ram <vickramodero6@gmail.com>
2024-02-27 12:24:38 -0800

    Repetition Token

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,2 @@
 #!/usr/bin/env ruby
-# The regular expression must match /h[ot]+/
-# Get the argument from the command line
-input = ARGV[0]
-# Define the regular expression
-regex = /h[ot]+/
-# Use the match method to check if the input matches the regular expression
-match = input.match(regex)
-# Print the match or a message if there is no match
-if match
-  puts match[0]
-else
-  puts "No match"
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join

4c11607d1e98a19d21df19e0bd1fc4a66438c080
vick-ram <vickramodero6@gmail.com>
2024-04-24 09:16:15 -0700

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,3 +1,5 @@
+# set up your client SSH configuration file so that
+# you can connect to a server without typing a password.
 file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/school',

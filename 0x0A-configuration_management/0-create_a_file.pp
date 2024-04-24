bb6588e08a5cc9460deed3c42f3110e12780fc22
vick-ram <vickramodero6@gmail.com>
2024-04-24 07:48:49 -0700

    Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Create a file
 file { '/tmp/school':
-    ensure   => file,
-    mode     => '0744',
-    owner    => 'www-data',
-    group    => 'www-data',
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
     content => 'I love Puppet\n',
 }

a6a8ed44510274d43b5a1837314f1ca1dec26d9a
vick-ram <vickramodero6@gmail.com>
2024-06-06 04:23:02 -0700

    code refactor

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,16 +7,12 @@ def number_of_subscribers(subreddit):
     """a function that returns number f subscribers from redit"""
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'MyApp/0.0.1'}
-
-    try:
-        response = requests.get(
-                        url,
-                        headers=headers,
-                        allow_redirects=False
-        )
-        if response.status_code == 200:
-            return response.json().get("data").get("subscribers")
-        else:
-            return 0
-    except Exception as e:
+    response = requests.get(
+                    url,
+                    headers=headers,
+                    allow_redirects=False
+    )
+    if response.status_code == 200:
+        return response.json().get("data").get("subscribers")
+    else:
         return 0

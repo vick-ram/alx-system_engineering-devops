eafa737dfb95f0d3349adec2ccdf2e865aad1abe
vick-ram <vickramodero6@gmail.com>
2024-06-06 04:34:29 -0700

    this 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,6 +15,7 @@ def number_of_subscribers(subreddit):
     res = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
         headers={"User-Agent": "MyApp/0.0.1"},
+        allow_redirects=False
     )
     if res.status_code == 404:
         return 0
